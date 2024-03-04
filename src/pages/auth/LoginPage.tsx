7303cf88a0d83c21c27b9fb1912c334c8700600e
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2024-03-04 16:35:17 +0100

    Add login with Cognito

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,4 +1,6 @@
+import { AuthenticationDetails, CognitoUser, CognitoUserPool } from 'amazon-cognito-identity-js';
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 interface Form {
     username: string;
@@ -6,6 +8,8 @@ interface Form {
 }
 
 const LoginPage = () => {
+    const navigate = useNavigate();
+
     const [form, setForm] = useState<Form>({
         username: '',
         password: ''
@@ -15,9 +19,43 @@ const LoginPage = () => {
         setForm({ ...form, [e.target.name]: e.target.value });
     };
 
+    const handleLogin = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const authenticationDetails = new AuthenticationDetails({
+            Username: form.username,
+            Password: form.password
+        });
+
+        const UserPoolId = import.meta.env.VITE_AWS_USER_POOL_ID;
+        const ClientId = import.meta.env.VITE_AWS_POOL_CLIENT_ID;
+
+        if (!UserPoolId || !ClientId) {
+            throw new Error('UserPoolId and ClientId are required');
+        }
+
+        const userPool = new CognitoUserPool({ UserPoolId, ClientId });
+
+        const userData = {
+            Username: form.username,
+            Pool: userPool
+        };
+
+        const cognitoUser = new CognitoUser(userData);
+        cognitoUser.authenticateUser(authenticationDetails, {
+            onSuccess: () => {
+                console.log('Success');
+                navigate('/');
+            },
+            onFailure: (err) => {
+                alert(err);
+            }
+        });
+    };
+
     return (
         <div className='grid h-full w-full place-items-center'>
-            <form className='flex flex-col gap-2 *:p-4 *:text-black'>
+            <form className='flex flex-col gap-2 *:p-4 *:text-black' onSubmit={handleLogin}>
                 <input name='username' placeholder='Username' onChange={handleChange} value={form.username} />
                 <input name='password' type='password' placeholder='Password' onChange={handleChange} value={form.password} />
                 <button type='submit'>Login</button>
