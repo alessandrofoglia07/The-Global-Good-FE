1666771bdca78bd4a3e1f39ebf826e933a1cd973
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2024-03-20 21:40:06 +0100

    Update landing section and add best sellers section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,7 +7,7 @@ const MainPage: React.FC = () => {
     return (
         <div id='MainPage' className='flex w-full flex-col items-center'>
             <Navbar />
-            <section className='relative h-[calc(80vh)] w-full'>
+            <section id='landing' className='relative h-[calc(80vh)] w-full'>
                 <img alt='nature-background' draggable='false' src={PeopleBg} className='absolute left-0 top-0 h-full min-w-[100vw] object-cover brightness-[0.6]' />
                 <div className='absolute left-1/2 top-1/2 flex h-max max-h-full -translate-x-1/2 -translate-y-1/2 flex-col items-center px-2 pb-20 -md:w-full -lg:w-3/4'>
                     <h1 className='pb-8 text-center text-5xl font-bold tracking-tight text-slate-100 md:text-6xl lg:text-7xl'>Beyond Shopping.</h1>
@@ -15,6 +15,18 @@ const MainPage: React.FC = () => {
                     <button className='mt-12 rounded-lg bg-slate-100 px-8 py-4 text-2xl uppercase tracking-wider transition-colors hover:bg-slate-200'>Support us now</button>
                 </div>
             </section>
+            <section id='bestsellers' className='w-full bg-slate-50 py-4'>
+                <h1 className='relative left-1/2 w-fit -translate-x-1/2 py-8 text-4xl tracking-tight'>Our Best Sellers</h1>
+                <div className='flex w-full flex-wrap justify-evenly pb-4 *:aspect-square *:w-72 *:rounded-full *:bg-slate-500'>
+                    {/* TODO: Add products later */}
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </section>
+            <section id='values'>{/* TODO */}</section>
             <section className='mt-48 flex flex-col items-center'>
                 {/* TODO: remove later on */}
                 <a href='/auth/signup'>Sign up page</a>
