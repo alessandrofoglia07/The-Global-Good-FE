9931bef7a90050f493f3a16d416efdceea9549b8
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2024-04-24 18:41:44 +0200

    Add default product card design

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -3,12 +3,26 @@ import { Product } from '@/types';
 import { toImgURL } from '@/utils/toImgURL';
 
 interface Props {
-    product: Product;
+    product: Product | undefined;
 }
 
 const ProductCard: React.FC<Props> = ({ product }: Props) => {
+    if (!product) {
+        return (
+            <div className='flex h-max w-full max-w-96 flex-col items-center rounded-lg p-6'>
+                <div className='mb-4 aspect-square h-60 animate-pulse rounded-lg bg-taupe/5' />
+                <div className='h-4 w-48 animate-pulse self-start rounded-full bg-taupe/20 text-left text-lg font-bold tracking-tight' />
+                <div className='mt-2 h-4 w-28 animate-pulse self-start rounded-full bg-taupe/40 tracking-tight' />
+                <div className='mt-1 flex w-full animate-pulse items-center justify-between'>
+                    <div className='h-4 w-16 rounded-full bg-taupe/40 font-semibold' />
+                    <div className='h-4 w-16 rounded-full bg-taupe/40 text-lg font-bold' />
+                </div>
+            </div>
+        );
+    }
+
     return (
-        <div className='flex h-max w-full max-w-96 flex-col items-center rounded-lg p-6'>
+        <div className='flex h-max w-full max-w-96 flex-col items-center rounded-lg p-6 focus-within:outline-none'>
             <a href={`/shop/${product.collection}/${product.name}`}>
                 <img draggable='false' src={toImgURL(product.img)} alt={product.name} className='aspect-square h-64 rounded-t-lg object-contain' />
             </a>
