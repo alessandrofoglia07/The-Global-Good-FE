107a208fccea3fc3d56736708d6de227a44e2474
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2024-04-30 16:29:20 +0200

    Add Rating, RatingButton components

---
+++ src/components/Rating.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Star from './Star';
+
+interface Props {
+    rating: number;
+    max?: number;
+    className?: string;
+}
+
+const Rating: React.FC<Props> = ({ rating, max = 5, className = '' }: Props) => {
+    return (
+        <div className={`items-centers flex ${className}`}>
+            {Array.from({ length: Math.floor(rating) }, (_, i) => (
+                <Star key={i} variant='filled' />
+            ))}
+            {!Number.isInteger(rating) && <Star variant='half' />}
+            {Array.from({ length: max - Math.ceil(rating) }, (_, i) => (
+                <Star key={i} variant='empty' />
+            ))}
+        </div>
+    );
+};
+
+export default Rating;
---
+++ src/components/RatingButton.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import Star from './Star';
+import { motion } from 'framer-motion';
+
+interface Props {
+    init: number;
+    max?: number;
+    className?: string;
+    onChange: (rating: number) => void;
+}
+
+const RatingButton: React.FC<Props> = ({ init, max = 5, className = '', onChange }: Props) => {
+    const [selectedRating, setSelectedRating] = useState(init);
+    const [hoveredRating, setHoveredRating] = useState(init);
+    const [hovering, setHovering] = useState(false);
+
+    const handleRatingClick = (newRating: number) => {
+        setSelectedRating(newRating);
+        onChange(newRating);
+    };
+
+    return (
+        <div
+            onMouseEnter={() => {
+                setHovering(true);
+            }}
+            onMouseLeave={() => {
+                setHovering(false);
+            }}
+            className={`flex items-center ${className}`}>
+            {Array.from({ length: max }, (_, i) => (
+                <motion.button
+                    whileHover={{ scale: 1.2 }}
+                    whileTap={{ scale: 0.8 }}
+                    key={i}
+                    onClick={() => handleRatingClick(i)}
+                    onMouseEnter={() => {
+                        setHoveredRating(i);
+                    }}
+                    type='button'>
+                    <Star variant={!hovering ? (i <= selectedRating ? 'filled' : 'empty') : i <= hoveredRating ? 'filled' : 'empty'} />
+                </motion.button>
+            ))}
+        </div>
+    );
+};
+
+export default RatingButton;
