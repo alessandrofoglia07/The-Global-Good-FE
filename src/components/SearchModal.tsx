6cbd4c3bf9e31c7ec4732dea13b2f88a3c6cd8ea
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2024-05-02 19:05:31 +0200

    Fix search modal

--- src/components/SearchModal.tsx
+++ src/components/SearchModal.tsx
@@ -3,13 +3,13 @@ import axios from '@/api/axios';
 import { Product } from '@/types';
 import { toImgURL } from '@/utils/toImgURL';
 import { toCollectionName } from '@/utils/toCollectionName';
+import { Dialog } from '@headlessui/react';
 
 interface Props {
     open: boolean;
     onClose: () => void;
 }
 
-// Fix scrolling
 const SearchModal: React.FC<Props> = ({ open, onClose }) => {
     const [search, setSearch] = useState('');
     const [products, setProducts] = useState<Product[]>([]);
@@ -29,12 +29,10 @@ const SearchModal: React.FC<Props> = ({ open, onClose }) => {
         fetchProducts(e.target.value);
     };
 
-    if (!open) return null;
-
     return (
-        <>
-            <div className='animate-fade-in fixed left-0 top-0 z-50 h-full w-full bg-black/20' onClick={onClose} />
-            <div className='animate-zoom-in-center fixed left-1/2 top-1/2 z-[60] max-h-[90vh] w-full -translate-x-1/2 -translate-y-1/2 transform rounded-lg bg-neutral-50 p-8 sm:w-[80vw] md:w-[50vw] lg:w-[40vw]'>
+        <Dialog open={open} onClose={onClose}>
+            <div className='fixed inset-0 z-[60] bg-black/25' />
+            <Dialog.Panel className='animate-zoom-in-center fixed left-1/2 top-1/2 z-[61] max-h-[90vh] w-full -translate-x-1/2 -translate-y-1/2 transform rounded-lg bg-neutral-50 p-8 sm:w-[80vw] md:w-[60vw] lg:w-[50vw]'>
                 <input
                     type='text'
                     placeholder='Type your search...'
@@ -43,24 +41,26 @@ const SearchModal: React.FC<Props> = ({ open, onClose }) => {
                     onChange={handleChange}
                     className='w-full rounded-lg border border-gray-100 p-2 text-taupe shadow-sm focus:outline-none'
                 />
-                <div className='mt-4 flex h-full w-full flex-col items-center space-y-4'>
-                    {products.map((product) => (
-                        <div key={product.name} className='flex min-h-28 w-full items-center gap-8 rounded-lg border border-gray-100 bg-white px-8 py-4 shadow-sm'>
-                            <a className='grid w-2/5 place-items-center lg:w-1/4'>
-                                <img src={toImgURL(product.img)} alt={product.name} className='max-h-24' />
-                            </a>
-                            <div className='flex h-full w-3/5 flex-col justify-center px-2 lg:w-3/4'>
-                                <p className='h-min text-sm text-gray-500'>{toCollectionName(product.collection)}</p>
-                                <a href={`/shop/${product.collection}/${product.name}`} className='h-min w-max max-w-[80%] text-lg font-bold tracking-tight'>
-                                    {product.name}
+                {products.length > 0 && (
+                    <div className='mt-4 flex h-full w-full flex-col items-center space-y-4'>
+                        {products.map((product) => (
+                            <div key={product.name} className='flex min-h-28 w-full items-center gap-8 rounded-lg border border-gray-100 bg-white px-8 py-4 shadow-sm'>
+                                <a className='grid w-2/5 place-items-center lg:w-1/4'>
+                                    <img src={toImgURL(product.img)} alt={product.name} className='max-h-24' />
                                 </a>
-                                <p className='h-min tracking-tight text-gray-500'>$ {product.price}</p>
+                                <div className='flex h-full w-3/5 flex-col justify-center px-2 lg:w-3/4'>
+                                    <p className='h-min text-sm text-gray-500'>{toCollectionName(product.collection)}</p>
+                                    <a href={`/shop/${product.collection}/${product.name}`} className='h-min w-max max-w-[80%] text-lg font-bold tracking-tight'>
+                                        {product.name}
+                                    </a>
+                                    <p className='h-min tracking-tight text-gray-500'>$ {product.price}</p>
+                                </div>
                             </div>
-                        </div>
-                    ))}
-                </div>
-            </div>
-        </>
+                        ))}
+                    </div>
+                )}
+            </Dialog.Panel>
+        </Dialog>
     );
 };
 
