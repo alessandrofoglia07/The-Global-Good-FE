5b6b8b8acb9fdd2ee3dc527c8bf4932750aa0398
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2024-05-11 22:53:12 +0200

    Refactor SearchModal component for responsive design

--- src/components/SearchModal.tsx
+++ src/components/SearchModal.tsx
@@ -44,11 +44,11 @@ const SearchModal: React.FC<Props> = ({ open, onClose }) => {
                 {products.length > 0 && (
                     <div className='mt-4 flex h-full w-full flex-col items-center space-y-4'>
                         {products.map((product) => (
-                            <div key={product.name} className='flex min-h-28 w-full items-center gap-8 rounded-lg border border-gray-100 bg-white px-8 py-4 shadow-sm'>
-                                <a href={`/shop/${product.collection}/${product.name}`} className='grid w-2/5 place-items-center lg:w-1/4'>
-                                    <img src={toImgURL(product.img)} alt={product.name} className='max-h-24' />
+                            <div key={product.name} className='flex min-h-28 w-full items-center gap-8 rounded-lg border border-gray-100 bg-white py-4 shadow-sm md:px-8 -md:flex-col'>
+                                <a href={`/shop/${product.collection}/${product.name}`} className='grid w-full place-items-center md:w-2/5 lg:w-1/4'>
+                                    <img src={toImgURL(product.img)} alt={product.name} className='max-h-32 md:max-h-24' />
                                 </a>
-                                <div className='flex h-full w-3/5 flex-col justify-center px-2 lg:w-3/4'>
+                                <div className='flex h-full w-full flex-col justify-center px-4 md:w-3/5 md:px-2 lg:w-3/4'>
                                     <p className='h-min text-sm text-gray-500'>{toCollectionName(product.collection)}</p>
                                     <a href={`/shop/${product.collection}/${product.name}`} className='h-min w-max max-w-[80%] text-lg font-bold tracking-tight'>
                                         {product.name}
