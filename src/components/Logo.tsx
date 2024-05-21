06340b2cabefa45306a5c104a12902c0ba78ac4c
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2024-05-21 21:46:03 +0200

    Update Logo component for responsive design

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const Logo = ({ className }: Props) => {
     return (
-        <h1 draggable='false' className={`flex select-none items-center gap-1 text-2xl font-extrabold tracking-tighter ${className}`}>
+        <h1 draggable='false' className={`flex select-none items-center gap-1 text-2xl font-extrabold tracking-tighter -md:text-xl -xs:text-lg ${className}`}>
             <HiOutlineGlobe className='text-3xl' /> TheGlobalGood
         </h1>
     );
